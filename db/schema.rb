ac6203f480a7804e1f047d1931476552d742fa3f
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-02-26 14:04:44 +0900

    feat: Room model を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_20_100028) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_21_161432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,17 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_20_100028) do
     t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.bigint "creator_id", null: false
+    t.string "name", limit: 50, null: false
+    t.string "room_key_digest"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_rooms_on_creator_id"
+    t.index ["name"], name: "index_rooms_on_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +78,5 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_20_100028) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "profiles", "users"
+  add_foreign_key "rooms", "users", column: "creator_id"
 end
---
+++ test/models/room_test.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id              :bigint           not null, primary key
+#  description     :text
+#  name            :string(50)       not null
+#  room_key_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  creator_id      :bigint           not null
+#
+# Indexes
+#
+#  index_rooms_on_creator_id  (creator_id)
+#  index_rooms_on_name        (name) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (creator_id => users.id)
+#
+require "test_helper"
+
+class RoomTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
