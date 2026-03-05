bad2c5080fda63fda35229719795ff4c79ef2bc5
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-03-05 15:56:10 +0900

    style: 日時の表示を動的にした

---
+++ app/decorators/schedule_decorator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ScheduleDecorator
+  def display_start_time
+    I18n.l(self.start_time, format: :short)
+  end
+
+  def display_end_time
+    I18n.l(self.end_time, format: :short)
+  end
+end
