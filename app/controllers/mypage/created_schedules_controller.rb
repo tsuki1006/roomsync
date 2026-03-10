a6fe8864331061f90752a99ca117452e3891e773
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-03-10 21:26:02 +0900

    feat: 作成・参加予定一覧を作成

--- app/controllers/mypage/created_schedules_controller.rb
+++ app/controllers/mypage/created_schedules_controller.rb
@@ -2,5 +2,6 @@ class Mypage::CreatedSchedulesController < Mypage::ApplicationController
   before_action :set_profile
 
   def index
+    @schedules = current_user.created_schedules.active.order(:start_time).includes(:creator, :room)
   end
 end
--- app/controllers/mypage/participated_schedules_controller.rb
+++ app/controllers/mypage/participated_schedules_controller.rb
@@ -2,5 +2,6 @@ class Mypage::ParticipatedSchedulesController < Mypage::ApplicationController
   before_action :set_profile
 
   def index
+    @schedules = current_user.participated_schedules.active.order(:start_time).includes(:creator, :room)
   end
 end
