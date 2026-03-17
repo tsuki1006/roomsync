fa196cf61d67befbc0a03959e5e7e9a55089ef0c
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-03-17 16:30:31 +0900

    fix: ページを検出するメソッドを修正

---
+++ app/helpers/page_detection_helper.rb
@@ -0,0 +1,16 @@
+module PageDetectionHelper
+
+  def controller_action?(controller, action = 'show')
+    controller_name == controller && action.include?(action_name)
+  end
+
+  def add_active_class(path)
+    binding.pry
+    path = path.split('?').first # remove path after ?
+    'active' if current_page?(path)
+  end
+
+  def add_active_class_2(controller, action)
+    'active' if controller_action?(controller, action)
+  end
+end
