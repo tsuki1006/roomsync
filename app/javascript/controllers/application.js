f33245b55d7b500e28866c57aca3ba95f888bb3d
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-03-24 15:44:55 +0900

    chore: stimulusのログ出力を開発環境でのみ行うように設定

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,7 +3,7 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = true
+application.debug = (window.location.hostname === 'localhost')
 window.Stimulus   = application
 
 export { application }
