93a65164edd1a12a13c28d2fb0164b4340e25501
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-04-03 12:40:13 +0900

    feat: ゲストログイン機能を追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,7 @@
-class SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
+  def guest_sign_in
+    user = User.find_by(email: 'guest@example.com')
+    sign_in user
+    redirect_to profile_path, notice: 'ゲストユーザーとしてログインしました'
+  end
 end
