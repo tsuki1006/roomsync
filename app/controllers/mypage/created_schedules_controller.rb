a9cded96f8827b1488e4dae33a31333a451cbdfa
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-04-07 14:59:12 +0900

    pref: マイページのパフォーマンスの改善

--- app/controllers/mypage/created_schedules_controller.rb
+++ app/controllers/mypage/created_schedules_controller.rb
@@ -2,6 +2,9 @@ class Mypage::CreatedSchedulesController < Mypage::ApplicationController
   before_action :set_profile
 
   def index
-    @schedules = current_user.created_schedules.active.order(:start_time).includes(:creator, :room)
+    @created_schedules = current_user.created_schedules.active
+                          .order(:start_time)
+                          .includes( :room, :participation_relationships)
+    @participated_schedules = current_user.participated_schedules.active
   end
 end
--- app/controllers/mypage/participated_schedules_controller.rb
+++ app/controllers/mypage/participated_schedules_controller.rb
@@ -2,6 +2,13 @@ class Mypage::ParticipatedSchedulesController < Mypage::ApplicationController
   before_action :set_profile
 
   def index
-    @schedules = current_user.participated_schedules.active.order(:start_time).includes(:creator, :room)
+    @created_schedules = current_user.created_schedules.active
+    @participated_schedules = current_user.participated_schedules.active
+                              .order(:start_time)
+                              .includes(
+                                :room,
+                                :participation_relationships,
+                                creator: { profile: { avatar_attachment: :blob } }
+                                )
   end
 end
